6c05cc7ed55d5c22ea67923aa3181a8e321300c7
motoharu-yano <muzhedgehog@list.ru>
2021-11-23 18:34:35 +0300

    fix test

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,6 +6,7 @@ cp ${RECIPE_DIR}/test_patches/horology.out ${SRC_DIR}/src/test/regress/expected/
 cp ${RECIPE_DIR}/test_patches/name.out ${SRC_DIR}/src/test/regress/expected/name.out
 cp ${RECIPE_DIR}/test_patches/enum.out ${SRC_DIR}/src/test/regress/expected/enum.out
 cp ${RECIPE_DIR}/test_patches/create_view.out ${SRC_DIR}/src/test/regress/expected/create_view.out
+cp ${RECIPE_DIR}/test_patches/join.out ${SRC_DIR}/src/test/regress/expected/join.out
 
 # avoid absolute-paths in compilers
 export CC=$(basename "$CC")
